792cd83e49d9bebddc5e787d3a8ec2e476c0759e
Phiphatphon Lee <pl51987@ALA.local>
2023-02-15 09:01:15 -0700

    updated

---
+++ script.js
@@ -0,0 +1,47 @@
+/*
+ * declare map as a global variable
+ */
+var map;
+
+/*
+ * use google maps api built-in mechanism to attach dom events
+ */
+google.maps.event.addDomListener(window, "load", function () {
+
+  /*
+   * create map
+   */
+  var map = new google.maps.Map(document.getElementById("map_div"), {
+    center: new google.maps.LatLng(33.2210429, -111.5729535),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  /*
+   * create infowindow (which will be used by markers)
+   */
+  var infoWindow = new google.maps.InfoWindow();
+
+  /*
+   * marker creater function (acts as a closure for html parameter)
+   */
+  function createMarker(options, html) {
+    var marker = new google.maps.Marker(options);
+    if (html) {
+      google.maps.event.addListener(marker, "click", function () {
+        infoWindow.setContent(html);
+        infoWindow.open(options.map, this);
+      });
+    }
+    return marker;
+  }
+
+  /*
+   * add markers to map
+   */
+
+  var marker1 = createMarker({
+    position: new google.maps.LatLng(33.2210429, -111.5729535),
+    map: map
+  }, "<h1>ALA school</h1><p>you can ask for more information here</p>");
+});
